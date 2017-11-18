9e51505cbf5b181f3187ba5696e6d19e93cc43ba
Sam Hart <criswellious@gmail.com>
2017-11-18 16:47:18 -0500

    okay python may be better

---
+++ make-motd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import json
+import subprocess
+
+def exec_cmd(cmd):
+    #print(cmd)
+    try:
+        output = subprocess.check_output(cmd, shell=True)
+        lines = output.split('\n')
+        lines = [l.strip() for l in lines]
+    except subprocess.CalledProcessError:
+         return None
+
+    if lines[-1] == '':
+        lines.pop()
+    return lines
+
+kernel_rel = exec_cmd("uname -r")
+kernel_ver = exec_cmd("uname -v")
+uptime = exec_cmd("uptime")
+failed_sshd_text = exec_cmd("journalctl -u sshd --since today -p err --no-pager -o json")
--- make-motd.sh
+++ make-motd.sh
@@ -3,6 +3,5 @@
 # Gather all the info
 KERNEL_REL=$(uname -r)
 KERNEL_VER=$(uname -v)
-
-echo "$KERNEL_REL $KERNEL_VER"
+UPTIME=$(uptime)
 
