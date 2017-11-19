0cf6b156c854eb9921cac0fdf4a31c46e35e10b5
Sam Hart <hartsn@gmail.com>
2017-11-18 20:26:45 -0500

    add requirements, start parsing sshd

--- make-motd.py
+++ make-motd.py
@@ -2,13 +2,15 @@
 
 import sys
 import os
-import json
+import jsonlines
 import subprocess
 
 def exec_cmd(cmd):
     #print(cmd)
     try:
         output = subprocess.check_output(cmd, shell=True)
+        if isinstance(output, bytes):
+            output = output.decode('utf-8')
         lines = output.split('\n')
         lines = [l.strip() for l in lines]
     except subprocess.CalledProcessError:
@@ -22,7 +24,17 @@ kernel_rel = exec_cmd("uname -r")
 kernel_ver = exec_cmd("uname -v")
 uptime = exec_cmd("uptime")
 
-failed_sshd = None
-failed_sshd = json.loads(''.join(exec_cmd("journalctl -u sshd --since today -p err --no-pager -o json")))
+fssh_reader = jsonlines.Reader(exec_cmd("journalctl -u sshd --since today -p err --no-pager -o json"))
 
-print(failed_sshd)
+sshd_errors = {}
+
+for obj in fssh_reader:
+    if obj['MESSAGE'].startswith('error: PAM: Authentication failure for'):
+        l = obj['MESSAGE'].split('error: PAM: Authentication failure for')
+        p = l[-1].split('from')
+        user = p[0].strip()
+        ip = p[1].strip()
+        if user in sshd_errors:
+            sshd_errors[user].append(ip)
+        else:
+            ssdh_errors[user] = [ ip ]
