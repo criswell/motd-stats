0e50dbcb09795e2fce583f00d28df7cd43d21b0f
Sam Hart <hartsn@gmail.com>
2017-11-19 18:09:10 -0500

    Finalize with what I pretty much like

--- make-motd.py
+++ make-motd.py
@@ -25,9 +25,10 @@ def exec_cmd(cmd):
         lines.pop()
     return lines
 
-kernel_rel = exec_cmd("uname -r")
-kernel_ver = exec_cmd("uname -v")
-uptime = exec_cmd("uptime")
+kernel_rel = ''.join(exec_cmd("uname -r"))
+kernel_ver = ''.join(exec_cmd("uname -v"))
+uptime = ''.join(exec_cmd("uptime"))
+avial_kernels = exec_cmd("rpm -q kernel-default")
 
 fssh_reader = jsonlines.Reader(exec_cmd("journalctl -u sshd --since today -p err --no-pager -o json"))
 
@@ -57,24 +58,54 @@ for k in sshd_errors.keys():
     s.append(SSHD_Stat(k, len(sshd_errors[k]), Counter(sshd_errors[k])))
 
 sshd_stats = sorted(s, key=lambda stat : stat.tries)
-print(sshd_stats)
+
+def get_line():
+    return "="*75
+
+def get_ssh():
+    m = []
+    m.append(get_line())
+    m.append("  FAILED SSH ATTEMPT STATS")
+    m.append(get_line())
+    i = 0
+    for s in sshd_stats:
+        m.append("* User: {0}\tTries: {1}".format(s.user, s.tries))
+        k = 0
+        for ip in s.counter:
+            m.append("\tIP: {0}\tTries: {1}".format(ip, s.counter[ip]))
+            k = k + 1
+            if k >= MAX_SSH_IPS:
+                break
+        i = i + 1
+        if i >= MAX_SSH_LIST:
+            break
+    return m
+
+def get_kernel():
+    m = []
+    m.append(get_line())
+    m.append("  KERNEL INFO")
+    m.append(get_line())
+
+    m.append("* Running Kernel: {0} {1}".format(kernel_rel, kernel_ver))
+    m.append("* Available kernel(s):")
+    for i in avial_kernels:
+        m.append("\t\t{0}".format(i))
+
+    return m
+
+def get_uptime():
+    return [
+            get_line(),
+            "  UPTIME : {0}".format(uptime),
+            get_line()
+        ]
 
 motd = []
-
-motd.append("FAILED SSH ATTEMPT STATS")
-motd.append("========================")
-i = 0
-for s in sshd_stats:
-    motd.append("* User: {0}\tTries: {1}".format(s.user, s.tries))
-    k = 0
-    for ip in s.counter:
-        motd.append("\tIP: {0}\tTries: {1}".format(ip, s.counter[ip]))
-        k = k + 1
-        if k >= MAX_SSH_IPS:
-            break
-    i = i + 1
-    if i >= MAX_SSH_LIST:
-        break
-
+motd.extend(get_kernel())
+motd.append("")
+motd.extend(get_ssh())
+motd.append("")
+motd.extend(get_uptime())
 for l in motd:
     print(l)
